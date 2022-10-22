d1940f3a4059c82a963b16a8281d6768f122c151
mukhriddin-dev <khodievm@gmail.com>
2022-10-22 09:20:10 +0500

    ok '

---
+++ module-3/lesson-1/script.js
@@ -0,0 +1,41 @@
+// document.write(77+"8")
+// alert(88**2)
+// confirm('saytdan chiqasizmi ?')
+// prompt('ismingizni yozing')
+
+// console.log("is working  ")
+// console.error('saytda muammo bor')
+// console.warn('Internet juda sekin')
+// console.info('sayt muoffaqiyatli yuklandi'*adsfghgjhk)
+
+// const person = {
+//    isname: "Asilbek",
+//    age: 17,
+//    job: "programmer"
+// }
+
+// console.table(person)
+
+
+
+// JavaScriptda 8 ta data type mavjud.
+
+// 2 ta turga bo'linadi 
+// Number =>  1.23 ,  -46 ,   0.00001 , 1234567 , 0,98
+// String => "1223" , '122222222' , 'p h p' , `Nodejs`
+// console.log("12"+"10");
+// console.log(false+true*6);
+
+
+// + , - , * , / , ** / % / ++ , -- 
+
+//ES6
+
+// console.log(2%4);
+// console.log(8%7);
+
+// var number=1
+
+// console.log(--number);
+// console.log(number)/;
+

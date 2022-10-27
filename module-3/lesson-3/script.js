c505bbe0b2ea799e91aa098d83ee77d1dd393fd0
mukhriddin-dev <khodievm@gmail.com>
2022-10-27 09:52:59 +0500

    module3 lesson4

---
+++ module-3/lesson-3/script.js
@@ -0,0 +1,105 @@
+"use strict";
+
+// Number , String , Boolean , Null , Undefined , BigInt , Symbol
+// Object
+
+
+// console.log(typeof null);   
+
+// var , let , const =>
+
+
+
+// {
+//    var sm=122;
+//    sm=333;
+//    var sm=666;
+// }
+
+// {
+//    let sm="javaScript";
+//     sm="React js";
+
+
+// }
+
+// const sm="PHP Yii2";
+// sm="NodeJS"
+//const  sm="Spring boot";
+
+
+
+// let num=10;
+
+// num =100;
+
+
+
+
+// console.log(num);
+
+//  ! => not
+
+// console.log(10!=="10");
+// let num=10.95
+
+// console.log(Math.ceil(num));
+// console.log(Math.floor(num));
+// console.log(Math.round(num)); 
+// console.log(Math.trunc(num));
+// console.log(Math.abs(num));
+// console.log(Math.trunc(Math.random()*100));
+
+
+// let isName=prompt('ismingiz nima ?');
+// let age=+prompt('yoshingiz ?')
+
+// console.log(isName, age);
+
+// console.log(typeof(isName));
+// console.log(typeof(age));
+
+
+// let data="javaScript";
+
+// const num=Number(data);
+
+// console.log(data);
+// console.log(1/0);
+
+// console.log(typeof(1/0));
+
+let day = +prompt("TEL RAQAM KODINI KIRITING ");
+
+if (day === 1 ) {
+
+   console.log('bugun ' + day);
+
+} else if (day === 2) {
+   console.log('bugun ' + day);
+} else if (day === 3) {
+
+   console.log('bugun ' + day);
+
+} else if (day === 4) {
+
+   console.log('bugun ' + day);
+
+} else {
+   console.log("Bunaqa kun yo'q ");
+}
+
+
+//M-1
+
+let a=+prompt('raqam kiriting');
+
+if(a>0){
+   console.log("musbat");
+}else if(a==0){
+   console.log("musbat ham emas manfiy ham");
+}else{
+   console.log("son manfiy");
+}
+
+// M-2
---
+++ module-3/lesson-4/script.js
@@ -0,0 +1,115 @@
+"use strict";
+
+// let a=+prompt('a');
+
+// let b=+prompt('b');
+
+
+
+// if( a%2==0 || b%2==0){
+//    console.log('JUFT SONLAR');
+// }else{
+//    console.log('SONLARDAN BIRI TOQ ');
+// }
+
+// let n = +prompt('n=');
+
+// switch (number) {
+
+//    case 1:
+//       console.log('DUSHANBA');
+//       break;
+//    case 2:
+//       console.log('SESHANBA');
+//       break;
+//    case 3:
+//       console.log('CHORSHANBA');
+//       break;
+//    case 4:
+//       console.log('PAYSHANBA');
+//       break;
+//    case 5:
+//       console.log('jUMA');
+//       break;
+//    case 6:
+//       console.log('SHANBA');
+//       break;
+//    case 7:
+//       console.log('YAKSHANBA');
+//       break;
+//    default:
+//       console.log("Bunday kun yo'q ");
+
+// }
+
+
+// const lang=prompt('dasturlash tili');
+
+// switch(lang){
+//    case "javaScript" : console.log("ANGULAR, REACT, VUE , NEXT"); break;
+//    case "java" : console.log("spring", "spa", "kiwi"); break;
+//    case "php" : console.log("Yii2" , "Laravel" , "Symfony"); break;
+
+//    default : console.log("topilmadi");
+// }
+
+// console.log("java"==="jAva");
+
+
+// // 12>n  ? console.log("XA") : console.log("YO'Q");
+// const fasl=1;
+
+// switch(fasl){
+//    case fasl: console.log(fasl+"oy:  "+"Qish");
+//    case fasl : console.log(fasl+1+"oy:  "+"Qish"); break;
+
+// }
+
+
+
+
+// let sum=1;
+// while(n<5){
+//
+//    sum=sum*n;
+// }
+
+// console.log(sum)
+
+// let n = 0;
+
+
+// do {
+
+//    n++;
+//    console.log(n)
+
+// } while (n<10);
+
+
+
+// 2 4 6 8 10
+
+// let n=0;
+// let sum=0;
+// while(n<100){
+//    n++
+//    if(n%2==0){
+//       console.log(n)
+//       sum+=n;
+//    }
+// }
+
+// console.log(sum)
+
+// let x =+prompt("m=");
+// let n=+prompt('n=')
+
+// for (let i = n; i < x; i++) {
+
+//    for (let m = 1; m <= 10; m++) {
+//       document.write( i + " x " + m + "=" +i*m+"<br>")
+
+//    }
+
+// }
